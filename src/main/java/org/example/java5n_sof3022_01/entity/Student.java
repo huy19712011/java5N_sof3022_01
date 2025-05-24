0e66b1ce80c3b541b4b38f08ca0e090019439262
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-05-24 15:47:27 +0700

    CRUD: validate, custom error messages

--- src/main/java/org/example/java5n_sof3022_01/entity/Student.java
+++ src/main/java/org/example/java5n_sof3022_01/entity/Student.java
@@ -3,6 +3,9 @@ package org.example.java5n_sof3022_01.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +21,14 @@ public class Student {
 
     @Id
     private Long id;
+
+    @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters")
     private String name;
+
+    //@Email(message = "Please enter a valid email address")
+    @Pattern(regexp = "^(.+)@(.+)$", message = "Invalid email address")
     private String email;
+
+    @Pattern(regexp = "^(0)\\d{9}$", message = "Invalid phone number")
     private String phone;
 }
